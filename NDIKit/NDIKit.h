b4c7a5b3c481507cfe7b1ff3e10e3ed2067e0e91
keenaudio <aaron@keenaudio.com>
2017-12-08 15:25:46 -0700

    Initial Commit

---
+++ NDIKit/NDIKit.h
@@ -0,0 +1,19 @@
+//
+//  NDIKit.h
+//  NDIKit
+//
+//  Created by Aaron Granick on 12/8/17.
+//  Copyright © 2017 Aaron Granick. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NDIKit.
+FOUNDATION_EXPORT double NDIKitVersionNumber;
+
+//! Project version string for NDIKit.
+FOUNDATION_EXPORT const unsigned char NDIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NDIKit/PublicHeader.h>
+
+
